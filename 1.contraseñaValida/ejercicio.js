3c632802b3bd9698edcf1f7198761cde5a356688
nandolopezsantaria@gmail.com <nandolopezsantaria@gamil.com>
2024-11-28 12:31:31 -0500

    ejercicio 1

---
+++ 1.contraseñaValida/ejercicio.js
@@ -0,0 +1,11 @@
+// escribe tu respuesta acá
+
+function contrasenaValida(contrasena) {
+    return contrasena === "2Fj(jjbFsuj" || contrasena === "eoZiugBf&g9";
+}
+
+// código de prueba
+console.log(contrasenaValida("2Fj(jjbFsuj")); // true
+console.log(contrasenaValida("eoZiugBf&g9")); // true
+console.log(contrasenaValida("hola")); // false
+console.log(contrasenaValida("")); // false
