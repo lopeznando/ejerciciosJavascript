148181db76851ee81a14335e353cc5c9305be9e9
nandolopezsantaria@gmail.com <nandolopezsantaria@gamil.com>
2024-11-28 12:34:56 -0500

    ejercicio 8

---
+++ 8.SumarRangoNumeros/ejercicio.js
@@ -0,0 +1,13 @@
+function sumarRango(inicio, fin) {
+    let suma = 0;
+    for (let i = inicio; i <= fin; i++) {
+      suma += i;
+    }
+    return suma;
+  }
+  
+  // código de prueba
+  console.log(sumarRango(0, 10)) // 55
+  console.log(sumarRango(12, 14)) // 39
+  console.log(sumarRango(5, 5)) // 5
+  
