0a55b7212263a1b21e16eb615862a89c97bf8d9f
nandolopezsantaria@gmail.com <nandolopezsantaria@gamil.com>
2024-11-28 12:33:05 -0500

    ejercicio 4

---
+++ 4.imprimirUnArreglo/ejercicios.js
@@ -0,0 +1,11 @@
+// escribe tu respuesta acá
+function imprimirArreglo(elementos) {
+    elementos.for(elemento => console.log(elemento));
+}
+
+// código de prueba
+console.log(imprimirArreglo(1, "Hola", 2, "Mundo"));
+// 1
+// Hola
+// 2
+// Mundo
